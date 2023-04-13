010edf1a150c2c630b2331d1ff672ee3cffd0ce1
Chethiya Kusal <chethiyakdis@gmail.com>
2023-04-13 15:08:35 +0530

    privacy page

---
+++ src/screens/Privacy.jsx
@@ -0,0 +1,106 @@
+import React from "react";
+import "../privacy.css";
+
+const PrivacyPolicy = () => {
+  return (
+    <div className="privacy-container">
+      <h1>Privacy Policy for Beatific Happiness Planner Chrome Extension</h1>
+      <p>
+        At Beatific Happiness Planner, we take user privacy seriously and are
+        committed to protecting the personal information collected through our
+        Chrome extension. This Privacy Policy outlines the types of data we
+        collect, how we use and protect it, and your rights in relation to this
+        data.
+      </p>
+      <h2>Information We Collect</h2>
+      <p>
+        When you use the Beatific Happiness Planner Chrome extension, we may
+        collect the following types of information:
+      </p>
+      <ol>
+        <li>
+          Name / Username: We may collect your name or username to personalize
+          your experience with the extension.
+        </li>
+        <li>
+          Email address: We may collect your email address to communicate with
+          you about updates, improvements, and other important information
+          related to the extension.
+        </li>
+        <li>
+          Geo location: We may collect your geo location in order to provide you
+          with weather information from openweathermap.org, which is used to
+          enhance the functionality of the extension.
+        </li>
+        <li>
+          Data entered in the daily planner: We may collect and store the data
+          that you enter in the daily planner section of the extension, which
+          includes your personal notes and entries.
+        </li>
+      </ol>
+      <h2>Use of Collected Information</h2>
+      <p>We use the collected information for the following purposes:</p>
+      <ul>
+        <li>
+          To provide and improve the functionality of the Beatific Happiness
+          Planner Chrome extension, including personalizing your experience,
+          providing weather information, and saving your daily planner entries.
+        </li>
+        <li>
+          To communicate with you about updates, improvements, and other
+          important information related to the extension via email.
+        </li>
+        <li>
+          To analyze and improve the performance and usage of the extension, and
+          to conduct research and gather insights to enhance user experience.
+        </li>
+      </ul>
+      <h2>Data Sharing</h2>
+      <p>
+        We do not share your personal information with any third-party companies
+        or individuals unless we have obtained your consent or are required to
+        do so by law. We may share aggregated and anonymized data for research
+        and analytical purposes, which does not personally identify you.
+      </p>
+      <h2>Data Security</h2>
+      <p>
+        We take appropriate measures to protect the personal information
+        collected through the Beatific Happiness Planner Chrome extension from
+        unauthorized access, alteration, disclosure, or destruction. However,
+        please be aware that no method of transmission over the internet or
+        electronic storage is completely secure, and we cannot guarantee
+        absolute security of your data.
+      </p>
+      <h2>Your Rights</h2>
+      <p>
+        You have the right to access, update, and delete your personal
+        information that we have collected through the Beatific Happiness
+        Planner Chrome extension. You can request to exercise these rights by
+        contacting us at the support email provided below.
+      </p>
+      <h2>Updates to Privacy Policy</h2>
+      <p>
+        We may update this Privacy Policy from time to time to reflect changes
+        in our practices or legal requirements. We will notify you of any
+        material changes to this Privacy Policy through the extension or via
+        email.
+      </p>
+      <h2>Contact Us</h2>
+      <p>
+        If you have any questions or concerns about our Privacy Policy or the
+        data we collect, please contact us at:
+      </p>
+
+      <h4>Email: kusal@beatific.co</h4>
+
+      <h4>Website: https://beatific.co</h4>
+
+      <p>
+        We appreciate your trust in using the Beatific Happiness Planner Chrome
+        extension and are committed to protecting your privacy.
+      </p>
+    </div>
+  );
+};
+
+export default PrivacyPolicy;

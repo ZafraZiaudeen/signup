03317dcabb790de3ec131011c4a148083b157d1d
Chethiya Kusal <chethiyakdis@gmail.com>
2023-05-02 22:02:33 +0530

    chrome update

--- src/api/extension.js
+++ src/api/extension.js
@@ -1,10 +1,12 @@
 const extension = {
   openLoginPage: (txId) => {
     try {
+      // eslint-disable-next-line no-undef
       chrome.runtime.sendMessage("nlefhoanajbkkbgclihfeklpimfmgbdm", {
         command: "openPage",
         openLoginPage: txId || "",
       });
+      // eslint-disable-next-line no-undef
       chrome.runtime.sendMessage("ammdbipcanekinkojengpemphaeienjk", {
         command: "openPage",
         openLoginPage: txId || "",

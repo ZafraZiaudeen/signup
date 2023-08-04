387f1650fd138d54a21e8ebf682ff548bf1300b7
Chethiya Kusal <chethiyakdis@gmail.com>
2023-08-04 22:11:59 +0530

    [#BTFC-19] 150 - Could we use this I think they are way cooler

--- src/screens/SignUp.jsx
+++ src/screens/SignUp.jsx
@@ -27,6 +27,10 @@ import priceBadge from "../images/badgeWithCheck.svg";
 import happyPerson1 from "../images/happyPerson1.jpg";
 import happyPerson2 from "../images/happyPerson2.jpg";
 import happyPerson3 from "../images/happyPerson3.jpg";
+
+import eyeOpen from "../images/eyeOpen.svg"
+import eyeClosed from "../images/eyeClosed.svg"
+
 import Loader from "../components/Loader";
 import payments from "../api/payments";
 import Pricing from "../api/pricingPlans";
@@ -624,16 +628,16 @@ const Child = ({
                     if (e.code === "Enter") handleEmailNext(e);
                   }}
                   value={email}
-                  // required
+                // required
                 />
                 <span className={styles.requiredMsg} style={{ left: "58%" }}>
                   {emptyEmail
                     ? "Email Required"
                     : invalidEmail
-                    ? "Invalid email address"
-                    : alreadyReg
-                    ? "This email has already been used to create an account with us!"
-                    : ""}
+                      ? "Invalid email address"
+                      : alreadyReg
+                        ? "This email has already been used to create an account with us!"
+                        : ""}
                   {alreadyReg && (
                     <span
                       className={styles.whiteError}
@@ -719,40 +723,9 @@ const Child = ({
             />
             <span className={styles.showPass} onClick={handleClick}>
               {passwordVisible ? (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className={styles.eye}
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                >
-                  <path
-                    stroke-linecap="round"
-                    strokeLinejoin="round"
-                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                  />
-                  <path
-                    stroke-linecap="round"
-                    strokeLinejoin="round"
-                    d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
-                  />
-                </svg>
+                <img src={eyeOpen} className={styles.eye} alt="" />
               ) : (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className={styles.eye}
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21"
-                  />
-                </svg>
+                <img src={eyeClosed} className={styles.eye} alt="" />
               )}
             </span>
           </div>

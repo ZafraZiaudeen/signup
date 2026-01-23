4dfc35f767dbaa59c1fd5e3ac14b2cc1a0db7b26
Zafra Ziaudeen <zafraziaudeen@gmail.com>
2026-01-23 18:50:10 +0530

    update the config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 const { REACT_APP_SERVER_URL, REACT_APP_STRIPE_SECRET, REACT_APP_ENV, REACT_APP_PRICE_ID, REACT_APP_DEV_PRICE } = process.env;
 
 module.exports = {
-  serverUrl: REACT_APP_SERVER_URL || "http://localhost:5000",
+  serverUrl: REACT_APP_SERVER_URL || "https://beatific-dev-backend.vercel.app",
   stripeSecret: REACT_APP_STRIPE_SECRET,
   devMode: REACT_APP_ENV === "development",
   devPrice: REACT_APP_DEV_PRICE,

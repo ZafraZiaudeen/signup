326a558370c0ef9aa1d034e3acd9dde013fc6ab5
Fathima Zafra Ziaudeen Ahamed <zafraziaudeen@gmail.com>
2026-02-02 22:47:07 +0530

    Update sign-in URL format in extension.js

--- src/api/extension.js
+++ src/api/extension.js
@@ -13,7 +13,7 @@ const extension = {
       });
     } catch (e) {
       const base = process.env.REACT_APP_EXTENSION_URL || "http://localhost:5173";
-      const url = base.replace(/\/$/, "") + "/sign-in";
+      const url = base.replace(/\/$/, "") + "/#/sign-in";
       window.open(url);
     }
   },
